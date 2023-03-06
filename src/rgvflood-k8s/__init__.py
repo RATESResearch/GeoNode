af450d925f37fd5f7b764f23ce7b23d37e9f9839
Andrew Ernest <anernest@ratesresearch.org>
2023-03-06 15:24:31 +0000

    incorporating geonode-project

---
+++ src/rgvflood-k8s/__init__.py
@@ -0,0 +1,2 @@
+""":term:`REONode` is an implementation of :term:`GeoNodeGCP`, incorporating :term:`REON` extensions and applications. :term:`RGVFlood` is a production implementation of :term:`REONode` incorporating extensions and applications specified for the :term:`RGVFlood` deployment. :term:`RGVFlood` can be deployed as a :term:`Docker` stack, or as a :term:`k8s` app. The :term:`k8s` app is deployed on a :term:`GKE` autopilot cluster, available online at <http://rgvflood.com>. This section describes the process of constructing the base app.
+"""
